d1a5ebde7ffad5bf97a22fdd339af8e326427def
Hampus Strate <stratehampus02@gmail.com>
2022-10-24 15:20:15 +0000

    Add number

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,27 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
-                let gametype = this.getAttribute("data-type");
-                alert(`You clicked ${gametype}`);
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
-function runGame() {
+function runGame(gameType) {
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gaemType === "addition") {
+        displayAdditionQuestion[num1, num2];
+    } else {
+        alert(`Unknown game type: ${gameType})`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer () {
@@ -35,8 +45,9 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
 }
 
 function displaySubtractQuestion() {
